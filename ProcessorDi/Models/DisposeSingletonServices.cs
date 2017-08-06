c68a416d13ebc4a1216c6ef3da8857665996ea99
Corey Smith <coreyas@gmail.com>
2017-08-06 14:46:29 -0400

    Add processor to dispose of singleton services at end of request

---
+++ ProcessorDi/Models/DisposeSingletonServices.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+
+namespace ProcessorDi.Models
+{
+  public class DisposeSingletonServices : HttpRequestProcessor
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+      var singletonInstanceService = GetScopedServiceFromHttpContext(args, Constants.SingletonInstanceServiceKey);
+      singletonInstanceService.Dispose();
+
+      var transientInstanceService = GetScopedServiceFromHttpContext(args, Constants.TransientInstanceServiceKey);
+      transientInstanceService.Dispose();
+    }
+
+    private static IScopedService GetScopedServiceFromHttpContext(HttpRequestArgs args, string httpContextKey)
+    {
+      return args.Context.Items[httpContextKey] as IScopedService;
+    }
+  }
+}
